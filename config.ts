e26b5af293245010e1d0a20567f71b8af1349523
Viba <vraghu24@uw.edu>
2025-10-18 18:31:31 -0700

    fix changes from commit

--- config.ts
+++ config.ts
@@ -7,12 +7,13 @@
  * These variables should be set in a .env file locally.
  */
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyAMmArUjA5Cro-PDpyabQGexs9KZ3MH7dU",
+  authDomain: "syncup-1360e.firebaseapp.com",
+  projectId: "syncup-1360e",
+  storageBucket: "syncup-1360e.firebasestorage.app",
+  messagingSenderId: "389802074435",
+  appId: "1:389802074435:web:70ee23b7a79a2da94c0ff6",
+  measurementId: "G-RKM68KLK0H"
 };
 
 /**
